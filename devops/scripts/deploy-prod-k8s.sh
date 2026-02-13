025befba5d1f3e5326017fc0b0d374ef8e158adc
Thomas Erick <thomaserick.dev@gmail.com>
2026-02-13 17:21:31 -0300

    feat: Deploy fase 4

--- devops/scripts/deploy-prod-k8s.sh
+++ devops/scripts/deploy-prod-k8s.sh
@@ -16,9 +16,6 @@ kubectl get namespace $NAMESPACE || kubectl apply -f $K8S_PATH/namespace.yaml
 echo "Aplicando secrets..."
 kubectl apply -f $K8S_PATH/newrelic-secret.yaml -n $NAMESPACE
 
-echo "Aplicando configmap..."
-kubectl apply -f $K8S_PATH/configmap.yaml -n $NAMESPACE
-
 echo "Subindo aplicação Spring Boot..."
 kubectl apply -f $K8S_PATH/deployment.yaml -n $NAMESPACE
 
